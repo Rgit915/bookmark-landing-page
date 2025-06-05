01dea1ab21f2d9d883676cda71313651563f31ff
Rgit915 <rora.goitom@gmail.com>
2025-06-04 23:59:28 -0400

    Add responsive footer section component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import ContactUs from "./ContactUs";
+import logo from "../assets/images/logo-white-bookmark.svg";
+
+const Footer = () => {
+  const links = ["Features", "Pricing", "Contact"];
+  return (
+    <footer className="flex flex-col justify-center items-center mt-10 w-full">
+      <ContactUs />
+      <div className="footer flex flex-col justify-center items-center gap-8 bg-neutral-blue-950 text-white w-full px-4 py-12">
+        <img src={logo} alt="bookmark logo" />
+        <nav className="links">
+          <ul className="flex flex-col justify-around items-center gap-4">
+            {links.map((link) => (
+              <li key={link}>
+                <a
+                  href="#"
+                  className=" text-xl uppercase font-normal text-[15px] text-center text-white hover:text-primary-red-400 cursor-pointer"
+                >
+                  {link}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="social-icons flex gap-8 ">
+          <i className="fa-brands text-white hover:text-primary-red-400 hover:scale-110 transition-transform duration-200 cursor-pointer fa-square-facebook"></i>
+          <i className="fa-brands text-white hover:text-primary-red-400 hover:scale-110 transition-transform duration-200 cursor-pointer fa-twitter"></i>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
